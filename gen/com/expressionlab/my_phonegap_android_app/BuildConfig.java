1c382fe62bd5e84554230b69ba46f873bee9c2a7
Hatem Mahmoud <hatem@expressionlab.com>
2013-01-15 16:16:14 +0200

    Created 'Android Project from Existing Code' in Eclipse

---
+++ gen/com/expressionlab/my_phonegap_android_app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.expressionlab.my_phonegap_android_app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
